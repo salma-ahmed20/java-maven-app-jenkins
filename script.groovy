97ff768d0b4357b730e3c198c0c5269bfad61450
salma <salma.ahmed0075@gmail.com>
2023-04-19 14:29:35 +0200

    updates

--- script.groovy
+++ script.groovy
@@ -1,16 +1,3 @@
-// def buildJar() {
-//     echo 'building the application...'
-//     sh 'mvn package'
-// }
-
-def buildImage() {
-    echo 'Building docker image'
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t salma101/java-maven-app:jma-2.0 .'
-        sh "echo $PASS| docker login -u $USER --password-stdin"
-        sh 'docker push salma101/java-maven-app:jma-2.0'
-    }
-}
 
 def deployApp() {
     echo 'deploying the application...'
