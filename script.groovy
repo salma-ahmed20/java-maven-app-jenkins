5c3d34a2bcff0d1efbecb397607106dee29694c1
salma <salma.ahmed0075@gmail.com>
2023-04-20 00:53:07 +0200

    add increment version to script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,14 @@
 
+def incrementVersion(){
+    echo 'incrementing app version...'
+    sh 'mvn build-helper:parse-version versions:set \
+       -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+        versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    VERSION_NUMBER = "$version-$BUILD_NUMBER"
+}
+
 def deployApp() {
     echo 'deploying the application...'
 }
